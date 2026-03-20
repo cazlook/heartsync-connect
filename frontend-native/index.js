1dabbd3f51ffb60136454f3f81c1712df3f2509f
cazlook <mangiacapre2@gmail.com>
2026-03-20 16:09:48 +0100

    fix(mobile): add react-native-gesture-handler import as first line in index.js - fixes iOS TurboModuleRegistry crash

--- frontend-native/index.js
+++ frontend-native/index.js
@@ -1,3 +1,6 @@
+// MUST be the very first import - required for react-native-gesture-handler on iOS
+import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 import App from './App';
+
 registerRootComponent(App);
